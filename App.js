d1c4aee1d2fa19938a7fed431e6081a7365d06d1
Jin <jsy9508@gmail.com>
2022-12-02 21:12:57 +0900

    bookmark

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import Root from 'router';  
+import React, { useEffect } from 'react';
+import Root from 'router';
 import * as Notificatios from 'expo-notifications'
+import '/config/BookmarkConfig'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
 
@@ -11,6 +13,37 @@ export default function App() {
       shouldSetBadge: false,
     }),
   });
+
+  const getBusBookmarkData = async () => {
+    try {
+      const res = await AsyncStorage.getItem("busBookmarkData");
+      console.log(res);
+      if (res) {
+        const obj = JSON.parse(res);
+        const arr = Object.keys(obj).map(function(k) {return obj[k]});
+
+        console.log("loaded");
+        global.busBookmarkData = new Array;
+        global.busBookmarkData = arr;
+        console.log(global.busBookmarkData);
+      }
+      else {
+        console.log("no data");
+        global.busBookmarkData = new Array;
+      }
+
+      // const resObj = JSON.parse(res);
+      console.log("loaded: " + global.busBookmarkData);
+    } catch (error) {
+      // busBookmarkData = [];
+      console.log("new arr00");
+    }
+  }
+
+  useEffect(() => {
+    getBusBookmarkData();
+    // AsyncStorage.clear();
+  }, []);
 
   return (
     <Root />
--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -1,24 +1,122 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, TouchableOpacity } from 'react-native'
 import { Octicons } from '@expo/vector-icons'
-import { setBookmark, delBookmark } from '../utils/bookmark';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import '/config/BookmarkConfig'
 
 const Bookmark = ({ data }) => {
-    const [bookmark, set] = useState(false);
+  const [bookmark, set] = useState(false);
+  var busStopBookmarkData = new Array;
+  // var busBookmarkData = new Array;
+  // const busBookmarkData = Object.v?
 
-    const isBookmark = () => {
-        set(!bookmark);
+  const isBookmark = (item, key) => {
+    console.log(bookmark);
+    if(bookmark) {
+      if(checkBookmark(item, key))
+      delBookmark(item, key);
+    }
+    else {
+      console.log("set");
+      setBookmark(item, key);
+    }
+    set(!bookmark);
+  }
+
+    const setBookmark = async (data, key) => {
+        try {
+          switch (key) {
+            case 'busBookmarkData':
+                if (!checkBookmark(data, key)) {
+                    global.busBookmarkData.push(data);
+                    await AsyncStorage.setItem(key, JSON.stringify(global.busBookmarkData));
+                    console.log("added");
+                }
+                break;
+            case 'busStopBookmarkData':
+                if (!checkBookmark(data, key)) {
+                    busStopBookmarkData.push(data);
+                    await AsyncStorage.setItem(key, JSON.stringify(busStopBookmarkData));
+                }
+                break;
+            default:
+              console.log('Wrong Key');
+          }
+      
+          console.log(data);
+        } catch (e) {
+          throw new Error('Failed to save ' + key);
+        }
+      };
+
+    const delBookmark = async (data, key) => {
+        try {
+          switch (key) {
+            case 'busBookmarkData':
+              for (var i = 0; i < global.busBookmarkData.length; i++) {
+                if (global.busBookmarkData[i].routeid === data.routeid) {
+                  global.busBookmarkData.splice(i, 1);
+                  i--;
+                  console.log("deleted");
+                }
+              }
+              await AsyncStorage.setItem(key, JSON.stringify(global.busBookmarkData));
+              break;
+      
+            case 'busStopBookmarkData':
+              for (var i = 0; i < busStopBookmarkData.length; i++) {
+                if (busStopBookmarkData[i].nodeid === data.nodeid) {
+                  busStopBookmarkData.splice(i, 1);
+                  i--;
+                }
+              } 
+              await AsyncStorage.setItem(key, JSON.stringify(busStopBookmarkData));
+              break;
+            default:
+              console.log('Wrong Key');
+          }
+        } catch (e) {
+          throw new Error('Failed to remove ' + key);
+        }
+      };
+
+    const checkBookmark = (data, key) => {
+      if(global.busBookmarkData){
+        try {
+            switch(key) {
+                case 'busBookmarkData':
+                    for (var i = 0; i < global.busBookmarkData.length; i++) {
+                        if (global.busBookmarkData[i].routeid === data.routeid) {
+                            set(true); // error
+                          return true;
+                        }
+                    }
+                    return false;
+                case 'busStopBookmarkData':
+                    for (var i = 0; i < busStopBookmarkData.length; i++) {
+                        if (busStopBookmarkData[i].nodeid === data.nodeid) {
+                          set(true);
+                            return true;
+                        }
+                    }
+                    return false;
+            }
+        } catch (e) {
+            throw new Error('Failed to Check');
+        }
+      }
+      else return true;
     };
 
     return (
         <View style={{ flex: 1, alignItems: 'flex-end', marginRight: 20 }} >
             <TouchableOpacity onPress={() => {
-                isBookmark(); bookmark ? delBookmark(data.item, data.key) : setBookmark(data.item, data.key)
+                isBookmark(data.item, data.key);
             }}>
                 <Octicons
-                    name={bookmark ? 'heart-fill' : 'heart'}
+                    name={checkBookmark(data.item, data.key) ? 'heart-fill' : 'heart'}
                     size={25}
-                    color="#ff6347"
+                    color="#ff6347"   
                 />
             </TouchableOpacity>
         </View>
@@ -26,3 +124,113 @@ const Bookmark = ({ data }) => {
 };
 
 export default Bookmark;
+
+// import React, { useState } from 'react'
+// import { View, TouchableOpacity } from 'react-native'
+// import { Octicons } from '@expo/vector-icons'
+
+// const Bookmark = ({ data, key }) => {
+//     const [bookmark, set] = useState(false);
+//     const busBookmarkData = [];
+//     const busStopBookmarkData = [];
+
+//     const getBookmark = async (key) => {
+//         try {
+//             switch (key) {
+//                 case 'busBookmarkData':
+//                     const busBookmark = await AsyncStorage.getItem('busBookmarkData');
+//                     break;
+//                 case 'busStopBookmarkData':
+//                     const busStopBookmark = await AsyncStorage.getItem('busStopBookmarkData');
+//                     break;
+//             }
+//         } catch (e) {
+//             throw new Error('Failed to load ' + key);
+//         }
+//       }
+    
+//     const setBookmark = async (data, key) => {
+//         try {
+//           switch (key) {
+//             case 'busBookmarkData':
+//                 busBookmarkData.push(data);
+//                 await AsyncStorage.setItem(key, JSON.stringify(busBookmarkData));
+//             break;
+      
+//             case 'busBookmarkData':
+//                 busStopBookmarkData.push(data);
+//                 await AsyncStorage.setItem(key, JSON.stringify(busStopBookmarkData));
+//             break;
+//             default:
+//               console.log('Wrong Key');
+//           }
+
+//           console.log(data);
+//         } catch (e) {
+//           throw new Error('Failed to save ' + key);
+//         }
+//       };
+      
+//       const delBookmark = async (data, key) => {
+//         try {
+//           switch (key) {
+//             case 'busBookmarkData':
+//               for (var i = 0; i < busBookmarkData.length; i++) {
+//                 if (busBookmarkData[i].routeid === data.routeid) {
+//                   busBookmarkData.splice(i, 1);
+//                   i--;
+//                 }
+//               }
+//               await AsyncStorage.setItem(key, JSON.stringify(busBookmarkData));
+//               break;
+      
+//             case 'busStopBookmarkData':
+//               for (var i = 0; i < res.length; i++) {
+//                 if (res[i].nodeid === data.nodeid) {
+//                   res.splice(i, 1);
+//                   i--;
+//                 }
+//               } 
+//               await AsyncStorage.setItem(key, JSON.stringify(res));
+//               break;
+//             default:
+//               console.log('Wrong Key');
+//           }
+//         } catch (e) {
+//           throw new Error('Failed to remove ' + key);
+//         }
+//       };
+      
+//       const checkBookmark = async (data, key) => {
+//         try {
+//           switch (key) {
+//             case 'busBookmarkData':
+//               // busBookmarkData.pop(data);
+//               break;
+      
+//             case 'busStopBookmarkData':
+//               // busStopBookmarkData.pop(data);
+//               break;
+//             default:
+//               console.log('Wrong Key');
+//           }
+//         } catch (e) {
+//           throw new Error('Failed to check ' + key);
+//         }
+//       };
+
+//     return (
+//         <View style={{ flex: 1, alignItems: 'flex-end', marginRight: 20 }} >
+//             <TouchableOpacity onPress={() => 
+//                 delBookmark(data, key)}>
+//                 <Octicons
+//                     name={'heart-fill'}
+//                     size={25}
+//                     color="#ff6347"
+//                 />
+//             </TouchableOpacity>
+//         </View>
+//     );
+// };
+
+// export default Bookmark;
